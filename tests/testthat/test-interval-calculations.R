fc37e86a54179005291cdc3c4fa0594afc10a5f2
Randall Pruim <rpruim@calvin.edu>
2021-01-16 07:00:03 +0000

    version 0.9.0

--- tests/testthat/test-interval-calculations.R
+++ tests/testthat/test-interval-calculations.R
@@ -1,5 +1,5 @@
 context("interval-calculations")
-library(mosaic)
+# library(mosaic)
 
 test_that("CI on quantitative variables give correct values and have right names", {
   t1 <- df_stats(~ hp, data = mtcars, mn = ci.mean, md = ci.median, sd = ci.sd, long_names = FALSE)
@@ -28,18 +28,20 @@ test_that("CI for sample proportions work.", {
   expect_equivalent(t2[c("six_center")], ref$estimate)
 })
 
-# test_that("CI for sample proportions work with long_names.", {
-#   t2 <- df_stats(~ cyl, data = mtcars, six = ci.prop(success = 6))
-#   ref <- stats::binom.test(table(mtcars$cyl != 6))
-#   expect_equivalent(t2[c("six_cyl_lower", "six_cyl_upper")],
-#                     as.numeric(ref$conf.int))
-#   expect_equivalent(t2[c("six_cyl_center")], ref$estimate)
-# })
+test_that("CI for sample proportions work with long_names.", {
+  t2 <- df_stats(~ cyl, data = mtcars, six = ci.prop(success = 6))
+  ref <- stats::binom.test(table(mtcars$cyl != 6))
+  expect_equivalent(t2[c("six_lower", "six_upper")],
+                    as.numeric(ref$conf.int))
+  expect_equivalent(t2[c("six_center")], ref$estimate)
+})
 
 test_that("Alternative methods for proportion CI work", {
-  t3 <- ci.prop(mtcars$cyl == 6, method = "Plus4")
-  ref <- mosaic::binom.test(mtcars$cyl == 6, ci.method = "Plus4")
-  expect_equivalent(t3[c(1, 3)], ref$conf.int)
-  expect_equivalent(t3[2], ref$estimate)
+  if (require(mosaic)) {
+    t3 <- ci.prop(mtcars$cyl == 6, method = "Plus4")
+    ref <- mosaic::binom.test(mtcars$cyl == 6, ci.method = "Plus4")
+    expect_equivalent(t3[c(1, 3)], ref$conf.int)
+    expect_equivalent(t3[2], ref$estimate)
+  }
 })
 
